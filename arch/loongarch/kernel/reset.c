80e1e9efd76230fbe50eb5efd21dec4ece6a990c
Huacai Chen <chenhuacai@loongson.cn>
2020-12-31 15:13:33 +0800

    LoongArch: Add kexec/kdump support
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/kernel/reset.c
+++ arch/loongarch/kernel/reset.c
@@ -4,6 +4,7 @@
  */
 #include <linux/kernel.h>
 #include <linux/acpi.h>
+#include <linux/cpu.h>
 #include <linux/efi.h>
 #include <linux/export.h>
 #include <linux/pm.h>
@@ -11,6 +12,9 @@
 #include <linux/reboot.h>
 #include <linux/delay.h>
 #include <linux/console.h>
+#include <linux/kexec.h>
+#include <linux/libfdt.h>
+#include <linux/of_fdt.h>
 
 #include <acpi/reboot.h>
 #include <asm/idle.h>
@@ -72,3 +76,66 @@ void machine_restart(char *command)
 		__arch_cpu_idle();
 	}
 }
+
+#ifdef CONFIG_KEXEC
+
+/* 0X80000000~0X80200000 is safe */
+#define KEXEC_CTRL_CODE	TO_CACHE(0x100000UL)
+#define KEXEC_BLOB_ADDR	TO_CACHE(0x108000UL)
+
+static int loongson_kexec_prepare(struct kimage *image)
+{
+	int i;
+	void *dtb = (void *)KEXEC_BLOB_ADDR;
+
+	for (i = 0; i < image->nr_segments; i++) {
+		if (!fdt_check_header(image->segment[i].buf)) {
+			memcpy(dtb, image->segment[i].buf, SZ_64K);
+			break;
+		}
+	}
+
+	/* kexec/kdump need a safe page to save reboot_code_buffer */
+	image->control_code_page = virt_to_page((void *)KEXEC_CTRL_CODE);
+
+	return 0;
+}
+
+static void loongson_kexec_shutdown(void)
+{
+#ifdef CONFIG_SMP
+	int cpu;
+
+	/* All CPUs go to reboot_code_buffer */
+	for_each_possible_cpu(cpu)
+		if (!cpu_online(cpu))
+			cpu_device_up(get_cpu_device(cpu));
+
+	secondary_kexec_args[0] = TO_UNCACHE(0x1fe01000);
+#endif
+	kexec_args[0] = fw_arg0;
+	kexec_args[1] = TO_PHYS(KEXEC_BLOB_ADDR);
+}
+
+static void loongson_crash_shutdown(struct pt_regs *regs)
+{
+	default_machine_crash_shutdown(regs);
+#ifdef CONFIG_SMP
+	secondary_kexec_args[0] = TO_UNCACHE(0x1fe01000);
+#endif
+	kexec_args[0] = fw_arg0;
+	kexec_args[1] = TO_PHYS(KEXEC_BLOB_ADDR);
+}
+
+static int __init loongarch_kexec_setup(void)
+{
+	_machine_kexec_prepare = loongson_kexec_prepare;
+	_machine_kexec_shutdown = loongson_kexec_shutdown;
+	_machine_crash_shutdown = loongson_crash_shutdown;
+
+	return 0;
+}
+
+arch_initcall(loongarch_kexec_setup);
+
+#endif
