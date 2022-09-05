crarch/linux

LoongArch: Add power management support
Add power management support for LoongArch: including CPUFreq, suspend
and hibernation.

Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

arch/loongarch/kernel/time.c
@@ -120,12 +120,17 @@ static unsigned long __init get_loops_per_jiffy(void)
 	return lpj;
 }
 
-static long init_timeval;
+static long init_offset __nosavedata;
+
+void save_counter(void)
+{
+	init_offset = drdtime();
+}
 
 void sync_counter(void)
 {
 	/* Ensure counter begin at 0 */
-	csr_writeq(-init_timeval, LOONGARCH_CSR_CNTC);
+	csr_writeq(init_offset, LOONGARCH_CSR_CNTC);
 }
 
 int constant_clockevent_init(void)
@@ -218,7 +223,7 @@ void __init time_init(void)
 	else
 		const_clock_freq = calc_const_freq();
 
-	init_timeval = drdtime() - csr_readq(LOONGARCH_CSR_CNTC);
+	init_offset = -(drdtime() - csr_readq(LOONGARCH_CSR_CNTC));
 
 	constant_clockevent_init();
 	constant_clocksource_init();

arch/loongarch/power/hibernate.c
@@ -1,0 +1,59 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <asm/fpu.h>
+#include <asm/sections.h>
+#include <asm/tlbflush.h>
+
+#include <loongson.h>
+
+static u64 saved_crmd;
+static u64 saved_prmd;
+static u64 saved_euen;
+static u64 saved_ecfg;
+struct pt_regs saved_regs;
+
+void save_processor_state(void)
+{
+	saved_crmd = csr_readl(LOONGARCH_CSR_CRMD);
+	saved_prmd = csr_readl(LOONGARCH_CSR_PRMD);
+	saved_euen = csr_readl(LOONGARCH_CSR_EUEN);
+	saved_ecfg = csr_readl(LOONGARCH_CSR_ECFG);
+
+	if (is_fpu_owner())
+		save_fp(current);
+}
+
+void restore_processor_state(void)
+{
+	csr_writel(saved_crmd, LOONGARCH_CSR_CRMD);
+	csr_writel(saved_prmd, LOONGARCH_CSR_PRMD);
+	csr_writel(saved_euen, LOONGARCH_CSR_EUEN);
+	csr_writel(saved_ecfg, LOONGARCH_CSR_ECFG);
+
+	if (is_fpu_owner())
+		restore_fp(current);
+}
+
+int pfn_is_nosave(unsigned long pfn)
+{
+	unsigned long nosave_begin_pfn = PFN_DOWN(__pa(&__nosave_begin));
+	unsigned long nosave_end_pfn = PFN_UP(__pa(&__nosave_end));
+
+	return	(pfn >= nosave_begin_pfn) && (pfn < nosave_end_pfn);
+}
+
+extern int swsusp_asm_suspend(void);
+
+int swsusp_arch_suspend(void)
+{
+	enable_pci_wakeup();
+	return swsusp_asm_suspend();
+}
+
+extern int swsusp_asm_resume(void);
+
+int swsusp_arch_resume(void)
+{
+	/* Avoid TLB mismatch during and after kernel resume */
+	local_flush_tlb_all();
+	return swsusp_asm_resume();
+}

arch/loongarch/power/suspend.c
@@ -1,0 +1,88 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * loongson-specific suspend support
+ *
+ * Author: Huacai Chen <chenhuacai@loongson.cn>
+ * Copyright (C) 2020 Loongson Technology Co., Ltd.
+ */
+#include <linux/acpi.h>
+#include <linux/pm.h>
+#include <linux/suspend.h>
+
+#include <asm/loongarchregs.h>
+#include <asm/setup.h>
+#include <asm/time.h>
+#include <asm/tlbflush.h>
+
+#include <loongson.h>
+
+u32 loongarch_nr_nodes;
+u64 loongarch_suspend_addr;
+u32 loongarch_pcache_ways;
+u32 loongarch_scache_ways;
+u32 loongarch_pcache_sets;
+u32 loongarch_scache_sets;
+u32 loongarch_pcache_linesz;
+u32 loongarch_scache_linesz;
+
+struct saved_registers {
+	u32 ecfg;
+	u32 euen;
+	u64 pgd;
+	u64 kpgd;
+	u32 pwctl0;
+	u32 pwctl1;
+};
+static struct saved_registers saved_regs;
+
+static void arch_common_suspend(void)
+{
+	save_counter();
+	saved_regs.pgd = csr_readq(LOONGARCH_CSR_PGDL);
+	saved_regs.kpgd = csr_readq(LOONGARCH_CSR_PGDH);
+	saved_regs.pwctl0 = csr_readl(LOONGARCH_CSR_PWCTL0);
+	saved_regs.pwctl1 = csr_readl(LOONGARCH_CSR_PWCTL1);
+	saved_regs.ecfg = csr_readl(LOONGARCH_CSR_ECFG);
+	saved_regs.euen = csr_readl(LOONGARCH_CSR_EUEN);
+
+	loongarch_nr_nodes = loongson_sysconf.nr_nodes;
+	loongarch_suspend_addr = loongson_sysconf.suspend_addr;
+	loongarch_pcache_ways = cpu_data[0].dcache.ways;
+	loongarch_scache_ways = cpu_data[0].scache.ways;
+	loongarch_pcache_sets = cpu_data[0].dcache.sets;
+	loongarch_scache_sets = cpu_data[0].scache.sets;
+	loongarch_pcache_linesz = cpu_data[0].dcache.linesz;
+	loongarch_scache_linesz = cpu_data[0].scache.linesz;
+}
+
+static void arch_common_resume(void)
+{
+	sync_counter();
+	local_flush_tlb_all();
+	csr_writeq(per_cpu_offset(0), PERCPU_BASE_KS);
+	csr_writeq(eentry, LOONGARCH_CSR_EENTRY);
+	csr_writeq(eentry, LOONGARCH_CSR_MERRENTRY);
+	csr_writeq(tlbrentry, LOONGARCH_CSR_TLBRENTRY);
+
+	csr_writeq(saved_regs.pgd, LOONGARCH_CSR_PGDL);
+	csr_writeq(saved_regs.kpgd, LOONGARCH_CSR_PGDH);
+	csr_writel(saved_regs.pwctl0, LOONGARCH_CSR_PWCTL0);
+	csr_writel(saved_regs.pwctl1, LOONGARCH_CSR_PWCTL1);
+	csr_writel(saved_regs.ecfg, LOONGARCH_CSR_ECFG);
+	csr_writel(saved_regs.euen, LOONGARCH_CSR_EUEN);
+}
+
+int loongarch_acpi_suspend(void)
+{
+	arch_common_suspend();
+
+	enable_gpe_wakeup();
+	enable_pci_wakeup();
+
+	/* processor specific suspend */
+	loongarch_suspend_enter();
+
+	arch_common_resume();
+
+	return 0;
+}

tangyouling/loongarch-next

arch/loongarch/kernel/time.c
@@ -115,12 +115,17 @@ static unsigned long __init get_loops_per_jiffy(void)
 	return lpj;
 }
 
-static long init_timeval;
+static long init_offset __nosavedata;
+
+void save_counter(void)
+{
+	init_offset = drdtime();
+}
 
 void sync_counter(void)
 {
 	/* Ensure counter begin at 0 */
-	csr_write64(-init_timeval, LOONGARCH_CSR_CNTC);
+	csr_write64(init_offset, LOONGARCH_CSR_CNTC);
 }
 
 static int get_timer_irq(void)
@@ -219,7 +224,7 @@ void __init time_init(void)
 	else
 		const_clock_freq = calc_const_freq();
 
-	init_timeval = drdtime() - csr_read64(LOONGARCH_CSR_CNTC);
+	init_offset = -(drdtime() - csr_read64(LOONGARCH_CSR_CNTC));
 
 	constant_clockevent_init();
 	constant_clocksource_init();

arch/loongarch/power/hibernate.c
@@ -1,0 +1,58 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <asm/fpu.h>
+#include <asm/loongson.h>
+#include <asm/sections.h>
+#include <asm/tlbflush.h>
+
+static u64 saved_crmd;
+static u64 saved_prmd;
+static u64 saved_euen;
+static u64 saved_ecfg;
+struct pt_regs saved_regs;
+
+void save_processor_state(void)
+{
+	saved_crmd = csr_read32(LOONGARCH_CSR_CRMD);
+	saved_prmd = csr_read32(LOONGARCH_CSR_PRMD);
+	saved_euen = csr_read32(LOONGARCH_CSR_EUEN);
+	saved_ecfg = csr_read32(LOONGARCH_CSR_ECFG);
+
+	if (is_fpu_owner())
+		save_fp(current);
+}
+
+void restore_processor_state(void)
+{
+	csr_write32(saved_crmd, LOONGARCH_CSR_CRMD);
+	csr_write32(saved_prmd, LOONGARCH_CSR_PRMD);
+	csr_write32(saved_euen, LOONGARCH_CSR_EUEN);
+	csr_write32(saved_ecfg, LOONGARCH_CSR_ECFG);
+
+	if (is_fpu_owner())
+		restore_fp(current);
+}
+
+int pfn_is_nosave(unsigned long pfn)
+{
+	unsigned long nosave_begin_pfn = PFN_DOWN(__pa(&__nosave_begin));
+	unsigned long nosave_end_pfn = PFN_UP(__pa(&__nosave_end));
+
+	return	(pfn >= nosave_begin_pfn) && (pfn < nosave_end_pfn);
+}
+
+extern int swsusp_asm_suspend(void);
+
+int swsusp_arch_suspend(void)
+{
+	enable_pci_wakeup();
+	return swsusp_asm_suspend();
+}
+
+extern int swsusp_asm_resume(void);
+
+int swsusp_arch_resume(void)
+{
+	/* Avoid TLB mismatch during and after kernel resume */
+	local_flush_tlb_all();
+	return swsusp_asm_resume();
+}

arch/loongarch/power/suspend.c
@@ -1,0 +1,87 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * loongson-specific suspend support
+ *
+ * Author: Huacai Chen <chenhuacai@loongson.cn>
+ * Copyright (C) 2020 Loongson Technology Co., Ltd.
+ */
+#include <linux/acpi.h>
+#include <linux/pm.h>
+#include <linux/suspend.h>
+
+#include <asm/loongarch.h>
+#include <asm/loongson.h>
+#include <asm/setup.h>
+#include <asm/time.h>
+#include <asm/tlbflush.h>
+
+u32 loongarch_nr_nodes;
+u64 loongarch_suspend_addr;
+u32 loongarch_pcache_ways;
+u32 loongarch_scache_ways;
+u32 loongarch_pcache_sets;
+u32 loongarch_scache_sets;
+u32 loongarch_pcache_linesz;
+u32 loongarch_scache_linesz;
+
+struct saved_registers {
+	u32 ecfg;
+	u32 euen;
+	u64 pgd;
+	u64 kpgd;
+	u32 pwctl0;
+	u32 pwctl1;
+};
+static struct saved_registers saved_regs;
+
+static void arch_common_suspend(void)
+{
+	save_counter();
+	saved_regs.pgd = csr_read64(LOONGARCH_CSR_PGDL);
+	saved_regs.kpgd = csr_read64(LOONGARCH_CSR_PGDH);
+	saved_regs.pwctl0 = csr_read32(LOONGARCH_CSR_PWCTL0);
+	saved_regs.pwctl1 = csr_read32(LOONGARCH_CSR_PWCTL1);
+	saved_regs.ecfg = csr_read32(LOONGARCH_CSR_ECFG);
+	saved_regs.euen = csr_read32(LOONGARCH_CSR_EUEN);
+
+	loongarch_nr_nodes = loongson_sysconf.nr_nodes;
+	loongarch_suspend_addr = loongson_sysconf.suspend_addr;
+	loongarch_pcache_ways = cpu_data[0].dcache.ways;
+	loongarch_scache_ways = cpu_data[0].scache.ways;
+	loongarch_pcache_sets = cpu_data[0].dcache.sets;
+	loongarch_scache_sets = cpu_data[0].scache.sets;
+	loongarch_pcache_linesz = cpu_data[0].dcache.linesz;
+	loongarch_scache_linesz = cpu_data[0].scache.linesz;
+}
+
+static void arch_common_resume(void)
+{
+	sync_counter();
+	local_flush_tlb_all();
+	csr_write64(per_cpu_offset(0), PERCPU_BASE_KS);
+	csr_write64(eentry, LOONGARCH_CSR_EENTRY);
+	csr_write64(eentry, LOONGARCH_CSR_MERRENTRY);
+	csr_write64(tlbrentry, LOONGARCH_CSR_TLBRENTRY);
+
+	csr_write64(saved_regs.pgd, LOONGARCH_CSR_PGDL);
+	csr_write64(saved_regs.kpgd, LOONGARCH_CSR_PGDH);
+	csr_write32(saved_regs.pwctl0, LOONGARCH_CSR_PWCTL0);
+	csr_write32(saved_regs.pwctl1, LOONGARCH_CSR_PWCTL1);
+	csr_write32(saved_regs.ecfg, LOONGARCH_CSR_ECFG);
+	csr_write32(saved_regs.euen, LOONGARCH_CSR_EUEN);
+}
+
+int loongarch_acpi_suspend(void)
+{
+	arch_common_suspend();
+
+	enable_gpe_wakeup();
+	enable_pci_wakeup();
+
+	/* processor specific suspend */
+	loongarch_suspend_enter();
+
+	arch_common_resume();
+
+	return 0;
+}